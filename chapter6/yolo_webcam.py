a43331dbad0df39390832cd0c8716263de70cda2
chhatra shah <shahkhesavchhatra@gmail.com>
2023-04-25 16:25:53 +0545

    using web cam

--- chapter6/yolo_webcam.py
+++ chapter6/yolo_webcam.py
@@ -2,6 +2,7 @@ import cv2
 from ultralytics import YOLO
 import cvzone
 import numpy as np
+import math
 #yolo with webcam
 
 #creating the web cam object
@@ -15,15 +16,36 @@ cap.set(4,720)
 # creating the model
 model= YOLO("../Yolo_Weights/yolov8n.pt")
 
+
 while True:
     success, img=cap.read()
     results=model(img,stream=True)
     for r in results:
         boxes=r.boxes
+
         for box in boxes:
+            #bounding box
             x1,y1,x2,y2=box.xyxy[0].numpy()
-            print(x1,y1,x2,y2)
-            cv2.rectangle(img,(int(x1),int(y1)),(int(x2),int(y2)),(255,0,255),5)
+            #x1,y1,w,h=box.xywh[0].numpy()
+            w,h=x2-x1,y2-y1
+            bbox=int(x1),int(y1),int(w),int(h)
+
+            conf=math.ceil(box.conf[0]*100)/100
+            print("the confidence is",conf)
+
+            cls=box.cls[0]
+
+            cvzone.cornerRect(img,bbox)
+            cvzone.putTextRect(img,f'{cls},{conf}',(max(0,int(x1)),max(35,int(y1))),scale=0.7,thickness=0.5)
 
     cv2.imshow("Image",img)
-    cv2.waitKey(1)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('x'):
+        break
+
+
+
+   # cv2.waitKey(1)
+
+cap.release()
+cv2.destroyAllWindows()
